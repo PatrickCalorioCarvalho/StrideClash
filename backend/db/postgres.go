9cf879e2f946d5e11098c997b24024d62ecf73ab
patrickcaloriocarvalho <patrick.calorio@hotmail.com>
2026-01-10 00:08:33 -0300

    gitignore

--- backend/db/postgres.go
+++ backend/db/postgres.go
@@ -1,17 +1,36 @@
 package db
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
 
-
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
 
 
 func Connect() *sql.DB {
+
+	if err := godotenv.Load(); err != nil {
+		log.Println("⚠️ .env não encontrado, usando variáveis do sistema")
+	}
+
+	user := os.Getenv("DB_USER")
+	pass := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	name := os.Getenv("DB_NAME")
+	ssl := os.Getenv("DB_SSLMODE")
+
+	dsn := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=%s",
+		user, pass, host, port, name, ssl,
+	)
+
 	db, err := sql.Open(
 		"postgres",
-		"postgres://postgres:*P4ssw0rd@localhost:5432/strideclash?sslmode=disable",
+		dsn,
 	)
 
 
--- backend/go.mod
+++ backend/go.mod
@@ -1,8 +1,10 @@
 module github.com/PatrickCalorioCarvalho/StrideClash/backend
+
 go 1.25.5
 
 require (
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	golang.org/x/net v0.47.0 // indirect
 	golang.org/x/sys v0.38.0 // indirect
